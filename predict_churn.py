b65f1ca8ad04f7f82a3b7152b6fbb409ebbb221e
pghdsk8 <dhallsk8@gmail.com>
2021-07-29 18:20:42 -0600

    added files for assignment

---
+++ predict_churn.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pycaret.classification import predict_model, load_model
+
+def load_data(filepath):
+    """
+    Loads diabetes data into a DataFrame from a string filepath.
+    """
+    df = pd.read_csv(filepath, index_col=0)
+    return df
+
+
+def make_predictions(df):
+    """
+    Uses the pycaret best model to make predictions on data in the df dataframe.
+    """
+    model = load_model('LDA')
+    predictions = predict_model(model, data=df)
+    predictions.rename({'Label': 'Churn_prediction'}, axis=1, inplace=True)
+    predictions['Churn_prediction'].replace({1: 'Churned', 0: 'Retained'},
+                                            inplace=True)
+    return predictions['Churn_prediction']
+
+
+if __name__ == "__main__":
+    df = load_data('C:/Users/Owner/Desktop/Regis University/Summer 2021/MSDS600 - Intro to Data Science/Week 5/new_churn_data.csv')
+    predictions = make_predictions(df)
+    print('predictions:')
+    print(predictions)
